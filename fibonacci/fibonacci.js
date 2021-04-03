aab8c86bcd54aff5b30bcf573f1e391955c68f87
vishnucm-web <80188821+vishnucm-web@users.noreply.github.com>
2021-04-03 21:29:31 +0530

    Update fibonacci.js
    Solved Fibonacci Series

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(limit) {
+  let a=0,b=1,c=0,fibStr='';
+  fibStr = a+","+b;
+  for(i=2;i<limit;i++)
+  {
+    c=a+b;
+    a=b;
+    b=c;
+    fibStr = c+",";
+  }
+  return fibStr;
 }
 
 module.exports = fibonacci
